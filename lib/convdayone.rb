7e7519630ef4b0bed7f460ee68476b503b3b0802
SHIOYA, Hiromu <kwappa.856@gmail.com>
2016-01-05 19:43:26 +0900

    :construction: implement to import from text files(WIP)

--- lib/convdayone.rb
+++ lib/convdayone.rb
@@ -1,3 +1,4 @@
 require 'convdayone/version'
 require 'convdayone/cli'
 require 'convdayone/moment_diary'
+require 'convdayone/text_files'
---
+++ lib/convdayone/text_files.rb
@@ -0,0 +1,93 @@
+require 'time'
+
+module Convdayone
+  class TextFiles
+    def self.import(dir)
+      filenames = Dir.glob(File.join(dir, '*.txt'))
+      filenames.sort.each do |filename|
+        data_file = DataFile.new(filename)
+        data =  data_file.parse
+        data.each do |d|
+          Convdayone::CLI.create(d[:text], date: d[:date])
+        end
+      end
+    end
+
+    class DataFile
+      def initialize(filename)
+        @name = filename
+        match = @name.match(/.+\/(?<year>\d{4})(?<month>\d{2}?)\.txt/)
+        @year = match[:year].to_i
+        @month = match[:month].to_i
+        @mode = :start
+        @date = nil
+        @buffer = []
+        @result = []
+      end
+
+      def parse
+        lines = File.open(@name, "r:BOM|UTF-8") { |f| f.read }
+
+        lines.each_line do |line|
+
+          case @mode
+
+          when :start
+            if check_start_line(line)
+              parse_md_date(line)
+            end
+
+          when :find_date
+            match = line.match(/(?<m>\d+)\/(?<day>\d+)/)
+            if match
+              @date = Time.local(@year, @month, match[:day].to_i, 23, 59)
+              @mode = :reading
+            end
+
+          when :reading
+            if check_start_line(line)
+              buffer_to_result
+              parse_md_date(line)
+            else
+              @buffer.push(line)
+            end
+          end
+        end
+
+        buffer_to_result
+        @result
+      end
+
+      def buffer_to_result
+        unless @buffer.empty?
+          @result.push({date: @date, text: @buffer.join})
+          @buffer = []
+        end
+      end
+
+      def parse_md_date(line)
+        if md_start_line?(line)
+          @date = Time.parse(line.gsub(/日\ .曜日\s/, ' ').gsub(/[年月]/, '/'))
+        end
+      end
+
+      def check_start_line(line)
+        if text_start_line?(line)
+          @mode = :find_date
+        elsif md_start_line?(line)
+          @mode = :reading
+        else
+          false
+        end
+      end
+
+      def text_start_line?(line)
+        line.strip.start_with?('=' * 8)
+      end
+
+      def md_start_line?(line)
+        line.strip.start_with?('- 20')
+      end
+    end
+  end
+end
