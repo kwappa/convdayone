a2e7821dbd0382f44b39efe42687a85a37c555b0
SHIOYA, Hiromu <kwappa.856@gmail.com>
2016-01-29 02:22:42 +0900

    :bug: fix broken test

--- test/test_convdayone_cli.rb
+++ test/test_convdayone_cli.rb
@@ -10,8 +10,8 @@ class TestConvdayoneCLI < Test::Unit::TestCase
 
       sub_test_case 'when option is given' do
         test 'returns correct cli option' do
-          options = { starred: true, date: '2015-01-06T12:34:56+09:00', invalid_key: 'invalid', photo_path: 'my/photo/path' }
-          assert_equal(Convdayone::CLI.cli_option(options), "--date='2015-01-06T12:34:56+09:00' --starred='true' --photo-path='my/photo/path'")
+          options = { starred: true, date: Time.parse('2015-01-06T12:34:56+09:00'), invalid_key: 'invalid', photo_path: 'my/photo/path' }
+          assert_equal(Convdayone::CLI.cli_option(options), "--date='2015-01-06 03:34:56' --starred='true' --photo-path='my/photo/path'")
         end
       end
     end
